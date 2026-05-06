d337713ca62386d8517eb55194f237595a23a15a
asat3chline <tutuaros@gmail.com>
2026-05-06 15:34:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+document.getElementById("joinBtn").addEventListener("click", function () {
+    window.location.href = "signup.html";
+});
+
+const data = ["Ankara Dress", "Casual Wear", "Luxury Gown", "Street Style"];
+
+document.getElementById("searchBtn").addEventListener("click", function () {
+    const query = document.getElementById("searchInput").value.toLowerCase();
+
+    const results = data.filter(item =>
+        item.toLowerCase().includes(query)
+    );
+
+    document.getElementById("results").innerHTML =
+        results.length > 0
+            ? results.join("<br>")
+            : "No styles found";
+});
+
+document.getElementById("loginBtn").addEventListener("click", function () {
+    alert("Login feature coming soon!");
+});
+
+function openDetails(styleName) {
+    localStorage.setItem("selectedStyle", styleName);
+    window.location.href = "details.html";
+}
